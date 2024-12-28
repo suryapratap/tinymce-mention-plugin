014e368779704fa21e25f0c9eb12a688739570de
saiprasad1586 <saiprasadgovindu@gmail.com>
2024-12-28 19:58:21 +0530

    fix issue

--- dist/plugin.min.js
+++ dist/plugin.min.js
@@ -215,7 +215,7 @@ class AutoComplete {
     this.editor.execCommand("mceInsertContent", false, this.options.insert(item, this.options));
   }
   offset() {
-    console.log("this.editor", this.editor.dom.getBoundingClientRect());
+    console.log("this.editor", this.editor);
     const rtePosition = this.editor.getBoundingClientRect();
     const contentAreaPosition = this.editor.getContentAreaContainer().getBoundingClientRect();
     const nodePosition = this.editor.dom.select("span#autocomplete")[0].getBoundingClientRect();
